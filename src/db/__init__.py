ac6b39ecf97ad02d200ea1c40fefbf924d92616a
rhawk117 <hawkinsry19@gmail.com>
2024-12-31 21:32:27 -0500

    chore: db changes

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,6 +1,6 @@
 from typing import AsyncGenerator
 from sqlalchemy.orm import DeclarativeBase
-from ..config.builds import settings
+from ..config.app_config import settings
 import os
 from ._db_internals import _DBInterface
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,24 +12,30 @@ __all__ = ["Base", "init_db", "get_session", "CRUDService"]
 class Base(DeclarativeBase):
     pass
 
-
-def _register_models() -> None:
-    '''imports all of the models to be registered to the database'''
-    from src.models import User
-
-
+    
 async def init_db() -> None:
     """Initialize the database and create tables."""
+    tables_exist = os.path.exists("instance")
     os.makedirs("instance", exist_ok=True)
     await _DBInterface.init_pragmas()
     engine = _DBInterface.get_engine()
     async with engine.begin() as connection:
-        _register_models()
+        from src.models import User
         await connection.run_sync(Base.metadata.create_all)
-
+    if not tables_exist:
+        await _init_db_defaults()
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     '''gets a async_sessionmaker from _DBInterface'''
     async_sess = _DBInterface.get_session_factory()
     async with async_sess() as session:
-        yield session
+        async with session.begin():
+            yield session
+
+async def drop_tables() -> None:
+    '''drops all tables in the database'''
+    if settings.DEBUG == False:
+        raise Exception("Cannot drop tables when DEBUG is 'False'")
+    engine = _DBInterface.get_engine()
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
